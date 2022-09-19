f8e3aaba45c5ba6b9b9c77622baa16b9304e51a8
Allison <allisontlngo@gmail.com>
2022-09-18 23:01:53 -0700

    Initial Commit

---
+++ BevvieUI/BevvieUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BevvieUIApp.swift
+//  BevvieUI
+//
+//  Created by Allison Ngo on 9/18/22.
+//
+
+import SwiftUI
+
+@main
+struct BevvieUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BevvieUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BevvieUI
+//
+//  Created by Allison Ngo on 9/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
